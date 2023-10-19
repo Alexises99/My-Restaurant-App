c320375daef5ebb88613d3eec232804b68b63dd0
Alex Sanchez <alexs@glue.digital>
2023-10-19 22:16:27 +0200

    add(static): fonts and colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': 'var(--primary-color)',
+        'secundary-color': 'var(--secundary-color)',
+        'background-color': 'var(--background-color)',
+        'dark-gray': 'var(--dark-gray)',
+      },
+    },
+    fontFamily: {
+      display: ['Saria', ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
 }
