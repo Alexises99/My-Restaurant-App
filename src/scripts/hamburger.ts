f0de6561d5103b05a3adea5e9967a86aa6255712
Alex Sanchez <alexs@glue.digital>
2023-10-31 20:38:42 +0100

    add(home): responsive home page

--- src/scripts/hamburger.ts
+++ src/scripts/hamburger.ts
@@ -1,11 +1,22 @@
+const closeAnimation = [{ height: '100vh' }, { height: '0vh' }]
+
+const animation = {
+  duration: 300,
+  iterations: 1,
+}
+
 document.addEventListener('astro:page-load', () => {
-  document.querySelector('.hamburger')?.addEventListener('click', () => {
+  document.querySelector('.hamburger')?.addEventListener('mousedown', () => {
     document.querySelector('.nav-links')?.classList.toggle('expanded')
     document.querySelector('.cross')?.classList.toggle('show')
   })
 
   document.querySelector('.cross')?.addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links') as HTMLElement
+    navLinks?.animate(closeAnimation, animation)
     document.querySelector('.cross')?.classList.toggle('show')
-    document.querySelector('.nav-links')?.classList.toggle('expanded')
+    setTimeout(() => {
+      navLinks?.classList.toggle('expanded')
+    }, 200)
   })
 })
